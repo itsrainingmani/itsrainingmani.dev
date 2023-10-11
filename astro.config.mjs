fc06c70294650f76cb8b04a4649448598c847cdf
Mani Sundararajan <mani@hey.com>
2023-10-11 17:11:43 -0400

    add tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://itsrainingmani.dev",
-	integrations: [mdx(), sitemap()],
+  site: "https://itsrainingmani.dev",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
