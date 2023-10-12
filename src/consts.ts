f48de74cd91d59fec5f911b0f69bf7028f28ca96
Mani Sundararajan <mani@hey.com>
2023-10-12 13:19:31 -0400

    simplify header and footer

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,10 @@
 
 export const SITE_TITLE = "Overthunk";
 export const SITE_DESCRIPTION = "Mani's personal website";
+
+export const SOCIAL_LINKS = {
+  github: "https://github.com/itsrainingmani/",
+  twitter: "https://twitter.com/itsrainingmani",
+  email: "jm75312@gmail.com",
+  replit: "https://replit.com/@itsrainingmani",
+};
