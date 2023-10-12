b782cf6fd5a9cea43af16e7ed45eb084e6de1b0b
Mani Sundararajan <mani@hey.com>
2023-10-12 00:12:23 -0400

    formatting again

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,31 +1,31 @@
 import { defineCollection, z } from "astro:content";
 
 function removeDupsAndLowerCase(array: string[]) {
-	if (!array.length) return array;
-	const lowercaseItems = array.map((str) => str.toLowerCase());
-	const distinctItems = new Set(lowercaseItems);
-	return Array.from(distinctItems);
+  if (!array.length) return array;
+  const lowercaseItems = array.map((str) => str.toLowerCase());
+  const distinctItems = new Set(lowercaseItems);
+  return Array.from(distinctItems);
 }
 
 const blog = defineCollection({
-	schema: z.object({
-		title: z.string().max(60),
-		description: z.string().min(50).max(160).optional(),
-		publishDate: z.coerce.date(),
-		tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
-		ogImage: z.string().optional(),
-		publish: z.boolean().default(false),
-	}),
+  schema: z.object({
+    title: z.string().max(60),
+    description: z.string().min(50).max(160).optional(),
+    publishDate: z.coerce.date(),
+    tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+    ogImage: z.string().optional(),
+    publish: z.boolean().default(false),
+  }),
 });
 
 const projects = defineCollection({
-	schema: z.object({
-		title: z.string().max(60),
-		description: z.string().min(20).max(160).optional(),
-		createdYear: z.string(),
-		tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
-		repo: z.string().url(),
-	}),
+  schema: z.object({
+    title: z.string().max(60),
+    description: z.string().min(20).max(160).optional(),
+    createdYear: z.string(),
+    tags: z.array(z.string()).default([]).transform(removeDupsAndLowerCase),
+    repo: z.string().url(),
+  }),
 });
 
 export const collections = { blog, project: projects };
