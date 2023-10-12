f4bff618585f17b99bb9f9ff00621ef3734e9acf
Mani Sundararajan <mani@hey.com>
2023-10-12 16:08:19 -0400

    fix image components & add assets folder

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://itsrainingmani.dev",
+  image: {
+    service: squooshImageService(),
+  },
   integrations: [mdx(), sitemap(), tailwind()],
 });
