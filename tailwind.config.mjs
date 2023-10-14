66c1b6c1e057bf6fd7b54327d12ef1c4dd4e8313
Mani Sundararajan <mani@hey.com>
2023-10-14 15:43:44 -0400

    tailwind typography plugin for styling md

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	corePlugins: {
+		// disabling some core plugins that are included in the css even when unused
+		touchAction: false,
+		ringOffsetWidth: false,
+		ringOffsetColor: false,
+		scrollSnapType: false,
+		borderOpacity: false,
+		textOpacity: false,
+	},
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		// typography enables styling for HTML rendered from Markdown
+		require("@tailwindcss/typography"),
+	],
 };
