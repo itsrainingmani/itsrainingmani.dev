838cc4938cae6ce908edd9a0d879b8c657b63452
Mani Sundararajan <mani@hey.com>
2023-10-13 20:28:09 -0400

    add icons to footer. fix table link sizing

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+import type { iconPaths } from "@/data/IconPaths";
 
 export const SITE_TITLE = "Overthunk";
 export const SITE_DESCRIPTION = "Mani's personal website";
@@ -7,6 +8,13 @@ export const SITE_DESCRIPTION = "Mani's personal website";
 export const SOCIAL_LINKS = {
 	github: "https://github.com/itsrainingmani/",
 	twitter: "https://twitter.com/itsrainingmani",
-	email: "jm75312@gmail.com",
+	email: "mailto:jm75312@gmail.com",
 	replit: "https://replit.com/@itsrainingmani",
 };
+
+/** Icon links to social media — edit these with links to your profiles! */
+export const ICON_LINKS: { label: string; href: string; icon: keyof typeof iconPaths }[] = [
+	{ label: "Twitter", href: SOCIAL_LINKS["twitter"], icon: "twitter-logo" },
+	{ label: "GitHub", href: SOCIAL_LINKS["github"], icon: "github-logo" },
+	{ label: "Email", href: SOCIAL_LINKS["email"], icon: "email" },
+];
