ed9f6e0ff60a2233a8e7b4e6bed34febfd3ad5fc
Mani Sundararajan <10191300+itsrainingmani@users.noreply.github.com>
2025-01-12 16:46:38 -0500

    use rehype-autolink-headings for making header links

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 import remarkToc from "remark-toc";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,11 +13,18 @@ export default defineConfig({
 	image: {
 		service: squooshImageService(),
 	},
+	markdown: {
+		rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
+			behavior: "prepend", content: { type: 'text', value: '# ' }, headingProperties: { className: ['anchor'] }, properties: {
+				className: ['anchor-link'],
+			},
+		}]],
+	},
 	integrations: [
 		mdx({
 			syntaxHighlight: "shiki",
-			shikiConfig: { theme: "github-dark-dimmed", wrap: true },
-			remarkPlugins: [remarkToc],
+			shikiConfig: { theme: "github-dark-dimmed", wrap: false },
+			remarkPlugins: [[remarkToc, { headings: "contents" }]],
 			gfm: true,
 		}),
 		sitemap(),
